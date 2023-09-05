afb1e013a1e4c7fc324a85007990e0da1079adab
james-mcdougall <jameswilliammcdougall@gmail.com>
2023-09-05 14:37:48 -0700

    MVP

---
+++ Pinch/Pinch/Data/PageData.swift
@@ -0,0 +1,14 @@
+//
+//  PageData.swift
+//  Pinch
+//
+//  Created by James McDougall on 9/5/23.
+//
+
+import Foundation
+
+
+let pagesData: [Page] = [
+    Page(id: 1, imageName: "magazine-front-cover"),
+    Page(id: 2, imageName: "magazine-back-cover")
+]
---
+++ Pinch/Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by James McDougall on 9/5/23.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
